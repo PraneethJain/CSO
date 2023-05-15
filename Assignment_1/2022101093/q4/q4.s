8d11d95a54a6bcd49ef425ad9095f1980df55a6d
PraneethJain <praneethjain005@gmail.com>
2023-05-15 15:50:09 +0530

    handled overflow

--- Assignment_1/2022101093/q4/q4.s
+++ Assignment_1/2022101093/q4/q4.s
@@ -4,6 +4,7 @@ get_product_assembly:
   movq $0, %r9                            # Set loop index as 0 
   movq $1, %rax                           # Set product as 1
   movq $1, %r10                           # Storing 1 in r10 for movq
+  movq $9223372036854775807, %r11         # LONG LONG MAX
 
   jmp .loop
 
@@ -12,7 +13,10 @@ get_product_assembly:
   je .done                                # If equal, then done
 
   incq %r9                                # Increment loop index
-  imulq (%rdi), %rax                      # Multiply with product
+  movq $0, %rdx
+  imulq (%rdi)                            # Product stored in rdx rax
+  idivq %r11                              # Taking module with LLMAX
+  movq %rdx, %rax                         # Remainder now stored in return value
   movq (%rdi, %r10, 8), %rdi              # Going to next node
   
   jmp .loop                               # Loop
