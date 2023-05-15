3dea8f67a7a98950b3d159328fdcf5fe4c6c9e8b
PraneethJain <praneethjain005@gmail.com>
2023-05-15 14:40:12 +0530

    all positive case

---
+++ Assignment_1/2022101093/q3/q3.s
@@ -0,0 +1,62 @@
+.global divide
+
+divide:
+  movq %rdi, %r9                      # m
+  movq %rsi, %r10                     # n
+  movq %rdx, %r11                     # arr
+
+  movq $0, %r14                       # Store quotient in r14 
+  movq $0, %r15                       # Store remainder in r15
+  
+  cmpq $0, %r10                       # Compare 0 with denomatinator
+  je .zero                            # If denominator is 0
+  jg .denominator_positive            # If denominator is positive
+  jl .denominator_negative            # If denominator is negative
+
+.zero:
+  movq $-1, %r14
+  movq $-1, %r15
+  jmp .done
+
+.denominator_positive:
+  cmpq $0, %r9                        # Compare 0 with numerator
+  je .all_zero                        # Return two zeros
+  jg .all_positive                    # Solve with both positive
+  jl .num_neg_den_pos                  # Solve with negative numerator and positive denominator
+
+.denominator_negative:
+  cmpq $0, %r9                       # Compare 0 with numerator
+  je .all_zero                        # Return two zeros
+  jg .num_pos_den_neg                 # Solve with positive numerator and negative denominator
+  jl .all_negative                    # Solve with both negative
+
+.all_zero:
+  movq $0, %r14
+  movq $0, %r15
+  jmp .done
+
+.all_positive:
+  cmpq %r10, %r9
+  jl .finish_all_positive
+
+  incq %r14
+  subq %r10, %r9
+
+  jmp .all_positive
+
+.finish_all_positive:
+  movq %r9, %r15
+  jmp .done
+
+.num_neg_den_pos:
+
+.num_pos_den_neg:
+
+.all_negative:
+
+.done:
+  movq $0, %r13                       # Make r13 index of array 
+  movq %r14, (%r11, %r13, 8)            # Set 0 index of array as 0
+  incq %r13                           # Increment the index
+  movq %r15, (%r11, %r13, 8)            # Set 1 index of array as 0
+  ret                                 # Return
